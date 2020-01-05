8becb4e9d76c9a6716ed5f347dbbd438ccc8d728
only3seconds <TripleP3@163.com>
2020-01-05 22:31:42 +0800

    quartz demo

---
+++ timing-go-scheduler/src/main/java/demo/MyScheduler.java
@@ -0,0 +1,36 @@
+package demo;
+
+import java.util.concurrent.TimeUnit;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class MyScheduler {
+
+  public static void main(String[] args) throws SchedulerException, InterruptedException {
+    // 1、创建调度器Scheduler
+    SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+    Scheduler scheduler = schedulerFactory.getScheduler();
+//    // 2、创建JobDetail实例，并与PrintWordsJob类绑定(Job执行内容)
+//    JobDetail jobDetail = JobBuilder.newJob(MyJob.class)
+//        .withIdentity("job1", "group1").build();
+//    // 3、构建Trigger实例,每隔1s执行一次
+//    Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "triggerGroup1")
+//        .startNow()//立即生效
+//        .withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * * ?")).build();//一直执行
+//
+//    //4、执行
+//    scheduler.scheduleJob(jobDetail, trigger);
+    System.out.println("--------scheduler start ! ------------");
+    scheduler.start();
+
+    //睡眠
+    TimeUnit.MINUTES.sleep(1);
+    scheduler.shutdown();
+    System.out.println("--------scheduler shutdown ! ------------");
+
+
+  }
+}
+
