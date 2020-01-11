544668d459d9417b031e56c1fbfb9db22d69c3c7
only3seconds <TripleP3@163.com>
2020-01-11 20:48:31 +0800

    update doc

--- timing-go-scheduler/src/main/java/demo/MyScheduler.java
+++ timing-go-scheduler/src/main/java/demo/MyScheduler.java
@@ -1,6 +1,8 @@
 package demo;
 
 import java.util.concurrent.TimeUnit;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
@@ -8,13 +10,14 @@ import org.quartz.impl.StdSchedulerFactory;
 
 public class MyScheduler {
 
+  private static final Logger LOGGER = LogManager.getLogger(MyScheduler.class);
+
   public static void main(String[] args) throws SchedulerException, InterruptedException {
     // 1、创建调度器Scheduler
     SchedulerFactory schedulerFactory = new StdSchedulerFactory();
     Scheduler scheduler = schedulerFactory.getScheduler();
-//    // 2、创建JobDetail实例，并与PrintWordsJob类绑定(Job执行内容)
-//    JobDetail jobDetail = JobBuilder.newJob(MyJob.class)
-//        .withIdentity("job1", "group1").build();
+//    // 2、创建JobDetail实例，并与MyJob类绑定(Job执行内容)
+//    JobDetail jobDetail = JobBuilder.newJob(MyJob.class).withIdentity("job1", "group1").build();
 //    // 3、构建Trigger实例,每隔1s执行一次
 //    Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "triggerGroup1")
 //        .startNow()//立即生效
@@ -22,13 +25,13 @@ public class MyScheduler {
 //
 //    //4、执行
 //    scheduler.scheduleJob(jobDetail, trigger);
-    System.out.println("--------scheduler start ! ------------");
+    LOGGER.info("--------scheduler start ! ------------");
     scheduler.start();
 
     //睡眠
     TimeUnit.MINUTES.sleep(1);
     scheduler.shutdown();
-    System.out.println("--------scheduler shutdown ! ------------");
+    LOGGER.info("--------scheduler shutdown ! ------------");
 
 
   }
